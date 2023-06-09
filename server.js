7c20e261826e21645cd837f0fa8eaa066e6ca49b
Tristen <tristenseng@gmail.com>
2023-06-08 19:06:04 -0700

    working api

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()  
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': '21',
+        'birthLocation': 'London, England',
+        'birthName': 'Sir Savage the 21st'
+    },
+    'chance the rapper': {
+        'age': '21',
+        'birthLocation': 'Chicago, Illnois',
+        'birthName': 'Chancellor Bennett'
+    },
+    'unknown': {
+        'age': 0,
+        'birthLocation': 'unknown',
+        'birthName': 'unknown'
+    }
+
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    let rapper = rappers[rapperName]
+    if (rapper) {
+        response.json(rapper)
+    }
+    else {
+        response.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT,() => {
+    console.log(`${PORT} is the port we're on.`)
+} )
